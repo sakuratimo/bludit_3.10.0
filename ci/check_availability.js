999627a3a49f4a803db0f9321ff34c87279ffa2a
xymcuc <787886660@qq.com>
2020-08-03 23:16:05 +0800

    change screen

--- ci/check_availability.js
+++ ci/check_availability.js
@@ -18,11 +18,11 @@ if (!fs.existsSync(dir)) {
   
     await page.goto(url);
     await page.screenshot({ path: dir + "/login.png" });
-    await page.type("#jsusername", "admin");
-    await page.type("#jspassword", "cuc123");
-    await page.click('button[type="submit"]', { delay: 3000 });
-    await page.waitFor(3000);
-    await page.screenshot({ path: dir + "/login_in.png" });
+    // await page.type("#jsusername", "admin");
+    // await page.type("#jspassword", "cuc123");
+    // await page.click('button[type="submit"]', { delay: 3000 });
+    // await page.waitFor(3000);
+    // await page.screenshot({ path: dir + "/login_in.png" });
   } catch (e) {
     console.log(e.toString());
     process.exit(1);
